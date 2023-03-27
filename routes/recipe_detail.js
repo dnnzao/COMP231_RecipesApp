f868e36dc2c607d745da292ddc039e86c3c458f5
Sharmaine11 <113381429+Sharmaine11@users.noreply.github.com>
2023-03-27 15:30:22 -0400

    added routing for recipe_detail
    edited index,js
    edited recipe_detail.js
    added edit_recipeform.ejs
    edited post.ejs

---
+++ routes/recipe_detail.js
@@ -0,0 +1,110 @@
+var express = require("express");
+var router = express.Router();
+
+// create a reference to the model
+let Recipe = require('../model/recipe');
+
+
+module.exports.displayRecipeDetail = (req, res, next) => {
+    Recipe.find((err, recipeDetail) => {
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            //console.log(RecipeDetail);
+
+            res.render('pages/recipe_detail', {title: 'Recipes', RecipeDetail: recipeDetail});      
+        }
+    });
+}
+
+module.exports.displayPostPage = (req, res, next) => {
+    res.render('pages/post', {title: 'Post Recipe'})          
+}
+
+module.exports.processPostPage = (req, res, next) => {
+    let newRecipe = Recipe({
+        "title": req.body.name,
+        "author": req.body.author,
+        "published": req.body.published,
+        "description": req.body.description,
+        "ingredients": req.body.ingredients
+    });
+
+    Recipe.create(newRecipe, (err, Recipe) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the recipe list
+            res.redirect('/list_recipes');
+        }
+    });
+
+}
+
+module.exports.displayEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    Recipe.findById(id, (err, recipeToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('recipe/edit_recipeform.ejs', {title: 'Edit Recipe', recipe: recipeToEdit})
+        }
+    });
+}
+
+module.exports.processEditPage = (req, res, next) => {
+    let id = req.params.id
+
+    let updatedRecipe = Recipe({
+        "_id": id,
+        "title": req.body.name,
+        "author": req.body.author,
+        "published": req.body.published,
+        "description": req.body.description,
+        "ingredients": req.body.ingredients
+    });
+
+    Recipe.updateOne({_id: id}, updatedRecipe, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            res.redirect('/list_recipes');
+        }
+    });
+}
+
+module.exports.performDelete = (req, res, next) => {
+    let id = req.params.id;
+
+    Recipe.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+             // refresh the book list
+             res.redirect('/list_recipes');
+        }
+    });
+}
+  
