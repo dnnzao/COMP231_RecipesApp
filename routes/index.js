011bfe90ab4425a75e949df3653bc816ca55ec72
Denio Barbosa Junior <deniojr@gmail.com>
2023-04-01 21:47:16 -0400

    Fixed list_recipes, created list_users created

--- routes/index.js
+++ routes/index.js
@@ -14,26 +14,27 @@ function isLoggedIn(req, res, next) {
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  res.render("home", { title: "COMP 231 - Assignment 1 - Home Page" });
+  res.render("home", { title: "COMP 231 - Group Project- Home Page" });
 });
 
 
 
 router.get("/list_recipes", async function (req, res, next) {
-  let recipeList = await Recipe.find();
+  const orderBy = req.query.order_recipes || "desc";
+  let recipeList = await Recipe.find().sort({ published : orderBy });
   const search = req.query.search;
 
   if (search) {
     const searchTerms = search.trim().toLowerCase().split(" ");
     recipeList = recipeList.filter((recipe) => {
-      return searchTerms.every(term => 
+      return searchTerms.some(term => 
         recipe.title.toLowerCase().includes(term) || 
         recipe.ingredients.toLowerCase().includes(term)
       );
     });
   }
   
-  res.render("list_recipes", { title: "List of Recipes", recipeList });
+  res.render("list_recipes", { title: "List of Recipes", recipeList , search, orderBy});
 });
 
 router.get("/recipe/:id", async function (req, res, next) {
@@ -45,7 +46,7 @@ router.get("/recipe/:id", async function (req, res, next) {
 router.get('/update', async (req, res, next) => {
   const id = req.query.userId;
   const recipe = await Recipe.findById(id);
-  res.render('updaterecipes', { title: "Recipe Update", recipe });
+  res.render('update_recipes', { title: "Recipe Update", recipe });
 });
 
 
@@ -62,8 +63,9 @@ router.get("/top_recipes", async function (req, res, next) {
 
 
 /* GET contact */
-router.get("/contact", async function (req, res, next) {
-    res.render("contact", { title: "COMP 231 - Assignment 1 - Contact" });
+router.get("/list_users", async function (req, res, next) {
+  let userList = await User.find();
+  res.render("list_users", { title: "List of Users", userList });
 });
 
 
